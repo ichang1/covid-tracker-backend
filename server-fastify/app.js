1ec8369a1d2b809a2e9ef69f2ca920fbd3d75e5f
ichang1 <isaac.chang717@gmail.com>
2021-06-28 23:21:25 -0400

    separated app and server

---
+++ server-fastify/app.js
@@ -0,0 +1,36 @@
+import fastifyCors from "fastify-cors";
+import fastifySwagger from "fastify-swagger";
+
+import { stateCovidRoutes, stateVaccineRoutes } from "./routes/state.js";
+import {
+  provinceCovidRoutes,
+  provinceVaccineRoutes,
+} from "./routes/province.js";
+import { countryCovidRoutes, countryVaccineRoutes } from "./routes/country.js";
+import { swaggerSchema } from "./schema/swagger.js";
+
+export function build(fastify, options, done) {
+  fastify.register(fastifyCors, {
+    origin: "*",
+  });
+
+  fastify.register(fastifySwagger, swaggerSchema);
+
+  const routes = [
+    {
+      route: stateCovidRoutes,
+      prefix: "covid-19",
+    },
+    { route: stateVaccineRoutes, prefix: "vaccine" },
+    { route: provinceCovidRoutes, prefix: "covid-19" },
+    { route: provinceVaccineRoutes, prefix: "vaccine" },
+    { route: countryCovidRoutes, prefix: "covid-19" },
+    { route: countryVaccineRoutes, prefix: "vaccine" },
+  ];
+
+  routes.forEach(({ route, prefix }, i) => {
+    fastify.register(route, { prefix });
+  });
+
+  done();
+}
--- server-fastify/server.js
+++ server-fastify/server.js
@@ -1,42 +1,10 @@
 import fastify from "fastify";
-import fastifyCors from "fastify-cors";
-import fastifySwagger from "fastify-swagger";
+import { build } from "./app.js";
 
-import { stateCovidRoutes, stateVaccineRoutes } from "./routes/state.js";
-import {
-  provinceCovidRoutes,
-  provinceVaccineRoutes,
-} from "./routes/province.js";
-import { countryCovidRoutes, countryVaccineRoutes } from "./routes/country.js";
-import { swaggerSchema } from "./schema/swagger.js";
+async function start(server, port = null) {
+  server.register(build);
+  const PORT = process.env.PORT || port || 4000;
 
-const server = fastify({ logger: true });
-
-server.register(fastifyCors, {
-  origin: "*",
-});
-
-server.register(fastifySwagger, swaggerSchema);
-
-const routes = [
-  {
-    route: stateCovidRoutes,
-    prefix: "covid-19",
-  },
-  { route: stateVaccineRoutes, prefix: "vaccine" },
-  { route: provinceCovidRoutes, prefix: "covid-19" },
-  { route: provinceVaccineRoutes, prefix: "vaccine" },
-  { route: countryCovidRoutes, prefix: "covid-19" },
-  { route: countryVaccineRoutes, prefix: "vaccine" },
-];
-
-routes.forEach(({ route, prefix }, i) => {
-  server.register(route, { prefix, description: i });
-});
-
-const PORT = process.env.PORT || 4000;
-
-async function start() {
   try {
     await server.listen(PORT);
   } catch (err) {
@@ -45,4 +13,5 @@ async function start() {
   }
 }
 
-start();
+const server = fastify({ logger: true });
+start(server);

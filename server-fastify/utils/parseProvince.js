94e576d67cb581dae9c58cbe1f711b52ca82b212
ichang1 <isaac.chang717@gmail.com>
2021-07-15 23:29:59 -0400

    small error in province vaccine parse

--- server-fastify/utils/parseProvince.js
+++ server-fastify/utils/parseProvince.js
@@ -1,16 +1,14 @@
-import { dateToNumber, dateToYesterday } from "./date.js";
+import {
+  dateToNumber,
+  dateToYesterday,
+  EARLIEST_COVID_DATE,
+  EARLIEST_VACCINE_DATE,
+  formatDate,
+  unformatDate,
+  dateInRange,
+} from "./date.js";
 
 export function parseJHUCSSEProvinceDate(data, date) {
-  const formatDate = (d) => {
-    const [month, day, year] = d.split("/");
-    return `${month}-${day}-${year + 2000}`;
-  };
-
-  const unformatDate = (d) => {
-    const [month, day, year] = d.split("-");
-    return `${month}/${day}/${year % 1000}`;
-  };
-
   const todayDate = unformatDate(date);
   const yesterdayDate = unformatDate(dateToYesterday(date));
 
@@ -18,18 +16,12 @@ export function parseJHUCSSEProvinceDate(data, date) {
   const todayTotalDeaths = data.timeline.deaths[todayDate];
   const todayTotalRecovered = data.timeline.recovered[todayDate];
 
-  const EARLIEST_COVID_DATE = "1-22-2020";
-  const hasValidYesterdayDate =
-    dateToNumber(formatDate(date)) >= dateToNumber(EARLIEST_COVID_DATE);
-  const yesterdayTotalCases = hasValidYesterdayDate
-    ? data.timeline.cases[yesterdayDate]
-    : 0;
-  const yesterdayTotalDeaths = hasValidYesterdayDate
-    ? data.timeline.deaths[yesterdayDate]
-    : 0;
-  const yesterdayTotalRecovered = hasValidYesterdayDate
-    ? data.timeline.recovered[yesterdayDate]
-    : 0;
+  const yesterdayTotalCases =
+    date === EARLIEST_COVID_DATE ? 0 : data.timeline.cases[yesterdayDate];
+  const yesterdayTotalDeaths =
+    date === EARLIEST_COVID_DATE ? 0 : data.timeline.deaths[yesterdayDate];
+  const yesterdayTotalRecovered =
+    date === EARLIEST_COVID_DATE ? 0 : data.timeline.recovered[yesterdayDate];
 
   const todayCases = todayTotalCases - yesterdayTotalCases;
   const todayDeaths = todayTotalDeaths - yesterdayTotalDeaths;
@@ -46,23 +38,12 @@ export function parseJHUCSSEProvinceDate(data, date) {
 }
 
 export function parseJHUCSSEProvinceCumulative(data, startDate, endDate) {
-  const formatDate = (d) => {
-    const [month, day, year] = d.split("/");
-    return `${month}-${day}-${parseInt(year) + 2000}`;
-  };
-
   const interestedDates = Object.keys(data.timeline.cases)
-    .filter(
-      (date) =>
-        dateToNumber(startDate) <= dateToNumber(formatDate(date)) &&
-        dateToNumber(endDate) >= dateToNumber(formatDate(date))
-    )
+    .filter((date) => dateInRange(date, startDate, endDate))
     .sort(
       (date1, date2) =>
         dateToNumber(formatDate(date1)) - dateToNumber(formatDate(date2))
     );
-
-  console.log(interestedDates);
 
   let cases = {};
   let deaths = {};
@@ -77,24 +58,9 @@ export function parseJHUCSSEProvinceCumulative(data, startDate, endDate) {
 }
 
 export function parseJHUCSSEProvinceDaily(data, startDate, endDate) {
-  const formatDate = (date) => {
-    const [month, day, year] = date.split("/");
-    return `${month}-${day}-${parseInt(year) + 2000}`;
-  };
-  const unformatDate = (date) => {
-    const [month, day, year] = date.split("-");
-    return `${month}/${day}/${parseInt(year) - 2000}`;
-  };
-  const dateInRange = (d) => {
-    const date = formatDate(d);
-    return (
-      dateToNumber(date) >= dateToNumber(startDate) &&
-      dateToNumber(date) <= dateToNumber(endDate)
-    );
-  };
   const allDates = Object.keys(data.timeline.cases);
   const datesInterested = allDates
-    .filter((date) => dateInRange(date))
+    .filter((date) => dateInRange(date, startDate, endDate))
     .sort((date1, date2) => {
       const a = dateToNumber(formatDate(date1));
       const b = dateToNumber(formatDate(date2));
@@ -106,14 +72,13 @@ export function parseJHUCSSEProvinceDaily(data, startDate, endDate) {
   let deaths = {};
   let recovered = {};
   let prevDate;
-  const EARLIEST_COVID_DATE = "1/22/20";
 
   datesInterested.forEach((date, idx) => {
     if (idx > 0) {
       prevDate = datesInterested[idx - 1];
     } else {
       // idx == 0
-      if (date === EARLIEST_COVID_DATE) {
+      if (date === unformatDate(EARLIEST_COVID_DATE)) {
         // there is no earlier date
         prevDate = "";
       } else {
@@ -121,6 +86,7 @@ export function parseJHUCSSEProvinceDaily(data, startDate, endDate) {
         prevDate = unformatDate(dateToYesterday(formatDate(date)));
       }
     }
+
     const casesTotalThisDay = data.timeline.cases[date];
     const casesTotalYesterday =
       prevDate === "" ? 0 : data.timeline.cases[prevDate];
@@ -156,35 +122,10 @@ export function parseRAPSProvinceDate(data, date) {
 }
 
 export function parseRAPSProvinceCumulative(data, startDate, endDate) {
-  const formatDate = (date) => {
-    const [month, day, year] = date.split("/");
-    return `${month}-${day}-${parseInt(year) + 2000}`;
-  };
-  const dateInRange = (d) => {
-    const date = formatDate(d);
-    return (
-      dateToNumber(date) >= dateToNumber(startDate) &&
-      dateToNumber(date) <= dateToNumber(endDate)
-    );
-  };
-  // const datesInterested = data.timeline
-  //   .filter(({ date: d }) => dateInRange(d))
-  //   .sort(({ date: date1 }, { date: date2 }) => {
-  //     const a = dateToNumber(formatDate(date1));
-  //     const b = dateToNumber(formatDate(date2));
-
-  //     return a - b;
-  //   });
-
-  // datesInterested.forEach(({ total, date }) => {
-  //   const hyphenDateFullYear = formatDate(date);
-  //   cumulativeData[hyphenDateFullYear] = total;
-  // });
-
   let cumulativeData = {};
   let curDate = endDate;
   let datesInterestedUnsorted = [];
-  while (dateInRange(curDate)) {
+  while (dateInRange(unformatDate(curDate), startDate, endDate)) {
     datesInterestedUnsorted.push(curDate);
     curDate = dateToYesterday(curDate);
   }
@@ -194,6 +135,8 @@ export function parseRAPSProvinceCumulative(data, startDate, endDate) {
 
     return a - b;
   });
+
+  //no vaccine data for provinces from this api so set 1o 0
   datesInterested.forEach((date) => {
     cumulativeData[date] = 0;
   });
@@ -201,35 +144,10 @@ export function parseRAPSProvinceCumulative(data, startDate, endDate) {
 }
 
 export function parseRAPSProvinceDaily(data, startDate, endDate) {
-  const formatDate = (date) => {
-    const [month, day, year] = date.split("/");
-    return `${month}-${day}-${parseInt(year) + 2000}`;
-  };
-  const dateInRange = (d) => {
-    const date = formatDate(d);
-    return (
-      dateToNumber(date) >= dateToNumber(startDate) &&
-      dateToNumber(date) <= dateToNumber(endDate)
-    );
-  };
-  // const datesInterested = data.timeline
-  //   .filter(({ date: d }) => dateInRange(d))
-  //   .sort(({ date: date1 }, { date: date2 }) => {
-  //     const a = dateToNumber(formatDate(date1));
-  //     const b = dateToNumber(formatDate(date2));
-
-  //     return a - b;
-  //   });
-
-  // datesInterested.forEach(({ daily, date }) => {
-  //   const hyphenDateFullYear = formatDate(date);
-  //   dailyData[hyphenDateFullYear] = daily;
-  // });
-
   let dailyData = {};
   let curDate = endDate;
   let datesInterestedUnsorted = [];
-  while (dateInRange(curDate)) {
+  while (dateInRange(unformatDate(curDate), startDate, endDate)) {
     datesInterestedUnsorted.push(curDate);
     curDate = dateToYesterday(curDate);
   }

999781c9931bc71c630f166af5a67713a2e7bed5
ichang1 <isaac.chang717@gmail.com>
2021-08-15 02:49:19 -0400

    took away https

--- server/schema/swagger.js
+++ server/schema/swagger.js
@@ -13,7 +13,6 @@ export const swaggerSchema = {
         url: "https://spdx.org/licenses/MIT.html",
       },
     },
-    schemes: ["https"],
     tags: [
       {
         name: "US state Covid-19 and vaccine",
